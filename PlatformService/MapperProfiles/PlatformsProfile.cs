0957da29673d597b917083aa5f132162b520da22
Станислав Наравецкас <xobbit26@gmail.com>
2023-02-03 23:33:57 +0200

    configured automapper

---
+++ PlatformService/MapperProfiles/PlatformsProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using PlatformService.Data.Entities;
+using PlatformService.DTOs;
+
+namespace PlatformService.MapperProfiles;
+
+public class PlatformsProfile : Profile
+{
+    public PlatformsProfile()
+    {
+        //Source -> Target
+        CreateMap<PlatformCreateDto, Platform>();
+        CreateMap<Platform, PlatformCreateDto>();
+    }
+}
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -10,6 +10,8 @@ builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
